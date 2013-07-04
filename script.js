54d20a2bc3917b8cffc08c428e34b57912375aad
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2013-07-04 09:40:41 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+/*jshint strict:true, noarg:true, noempty:true, eqeqeq:true, bitwise:true, undef:true, unused:true, nonew:true, browser:true, devel:true, boss:true, curly:false, immed:false, latedef:true, newcap:true, plusplus:false, trailing:true, debug:false, asi:false, evil:false, expr:true, eqnull:false, esnext:false, funcscope:false, globalstrict:false, loopfunc:false */
+
+(function($){
+  "use strict";
+  
+  var
+    $list=$('.btns').find('li'),
+    $links=$list.find('a');
+
+  $links.address();
+
+  
+})(window.jQuery);
